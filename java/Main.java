d99776f2cc5dfaea906e39c58b63c73e3e9ee389
Nicole Andrews <andre22n@mtholyoke.edu>
2017-07-12 14:28:06 -0400

    added candies and print statement files

---
+++ java/Main.java
@@ -0,0 +1,8 @@
+import def.simplemodule.PrintStatement;
+public class Main {
+	public static void main(String[] args) {
+		// PrintStatement statement = new PrintStatement();
+		// statement.print("hi");
+		new PrintStatement().print("hi");
+	}
+}
---
+++ java/print.js
@@ -0,0 +1,6 @@
+
+PrintStatement = function () {
+  this.print = function(statement) {
+    console.log(statement);
+  };
+}
