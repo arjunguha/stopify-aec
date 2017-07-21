432614751ad4dbb4b7456f79b167f41f1bc0fca8
Rachit Nigam <rachit.nigam12@gmail.com>
2017-07-21 17:24:25 -0400

    Use pyvirtualdriver in selenium driver.

--- driver.py
+++ driver.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
-from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pyvirtualdisplay import Display
 import sys
 import os
 
 filename = sys.argv[1]
 loc = os.getcwd()
 html_file = "file://" + os.path.join(loc, filename)
+
+display = Display(visible=0, size=(800, 600))
+display.start()
 
 try:
     # Create a new instance of the Firefox driver
@@ -17,7 +21,7 @@ try:
     driver.get(html_file)
 
     # Runner signals completion or failure by changing title to done
-    WebDriverWait(driver, 10).until(EC.title_contains("done"))
+    WebDriverWait(driver, 100).until(EC.title_contains("done"))
 
     # You should see "cheese! - Google Search"
     data = driver.find_element_by_id('data').get_attribute('innerHTML')
