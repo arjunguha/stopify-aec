897d3d3695e8bf47b7acbcac4a16fe9865a08806
Sam Baxter <sbaxter@cs.umass.edu>
2017-08-31 17:23:03 -0400

    More cljs benches

--- clojurescript/project.clj
+++ clojurescript/project.clj
@@ -1,10 +1,11 @@
 (defproject lejure "0.1.0-SNAPSHOT"
   :description "Pure ClojureScript benchmarks"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.908"]]
+                 [org.clojure/clojurescript "1.9.908"]
+                 [cljx-sampling "0.1.0"]]
   :plugins [[lein-cljsbuild "1.1.7"]]
   :hooks [leiningen.cljsbuild]
-  :source-paths ["nbody" "binarytrees" "fannkuch" "pidigits" "spectralnorm"]
+  :source-paths ["nbody" "binarytrees" "fannkuch" "pidigits" "spectralnorm" "meteor" "memuse" "collatz"]
   :clean-targets
   [[:cljsbuild :builds 0 :compiler :output-to]
    :target-path
@@ -65,4 +66,37 @@
                                                  :optimizations :advanced
                                                  :output-to "js-build/spectralnorm.js"
                                                  :output-dir "out/spectralnorm"
-                                                 :static-fns true}}}})
+                                                 :static-fns true}}
+                       :meteor {:id "dev"
+                                :jar true
+                                :source-paths ["meteor"]
+                                :incremental true
+                                :compiler {
+                                           :cache-analysis true
+                                           :parallel-build true
+                                           :optimizations :advanced
+                                           :output-to "js-build/meteor.js"
+                                           :output-dir "out/meteor"
+                                           :static-fns true}}
+                       :memuse {:id "dev"
+                                :jar true
+                                :source-paths ["memuse"]
+                                :incremental true
+                                :compiler {
+                                           :cache-analysis true
+                                           :parallel-build true
+                                           :optimizations :advanced
+                                           :output-to "js-build/memuse.js"
+                                           :output-dir "out/memuse"
+                                           :static-fns true}}
+                       :collatz {:id "dev"
+                                 :jar true
+                                 :source-paths ["collatz"]
+                                 :incremental true
+                                 :compiler {
+                                            :cache-analysis true
+                                            :parallel-build true
+                                            :optimizations :advanced
+                                            :output-to "js-build/collatz.js"
+                                            :output-dir "out/collatz"
+                                            :static-fns true}}}})
