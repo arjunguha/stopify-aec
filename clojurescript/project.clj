03b3e34c261435d0f894f665011346ff5aadcb4a
Sam Baxter <sbaxter@cs.umass.edu>
2017-08-31 13:32:56 -0400

    Add nbody clojurescript benchmark

---
+++ clojurescript/project.clj
@@ -0,0 +1,24 @@
+(defproject lejure "0.1.0-SNAPSHOT"
+  :description "Sample cljs project configuration"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.908"]]
+  :plugins [[lein-cljsbuild "1.1.7"]]
+  :hooks [leiningen.cljsbuild]
+  :source-paths ["nbody"]
+  :clean-targets
+  [[:cljsbuild :builds 0 :compiler :output-to]
+   :target-path
+   :compile-path]
+  :profiles {:uberjar {:aot :all}}
+  :cljsbuild {
+              :builds [{:id "dev"
+                        :jar true
+                        :source-paths ["nbody"]
+                        :incremental true
+                        :compiler {
+                                   :cache-analysis true
+                                   :parallel-build true
+                                   :optimizations :advanced
+                                   :output-to "out/main.js"
+                                   :output-dir "out"
+                                   :static-fns true}}]})
