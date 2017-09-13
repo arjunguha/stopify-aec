29e90b219c20aa1a7d00545d315c9764329f450f
Rachit Nigam <rachit.nigam12@gmail.com>
2017-09-13 12:25:28 -0400

    Minify all built files, add minifier script.

--- javascript/js-build/bitops-bitwise-and.js
+++ javascript/js-build/bitops-bitwise-and.js
@@ -21,15 +21,4 @@
  * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-let bitwiseAndValue = 4294967296;
-for (var i = 0; i < 600000; i++)
-    bitwiseAndValue = bitwiseAndValue & i;
-
-var result = bitwiseAndValue;
-
-var expected = 0;
-if (result != expected)
-    throw "ERROR: bad result: expected " + expected + " but got " + result;
-
+ */let bitwiseAndValue=4294967296;for(var i=0;i<600000;i++)bitwiseAndValue=bitwiseAndValue&i;var result=bitwiseAndValue;var expected=0;if(result!=expected)throw"ERROR: bad result: expected "+expected+" but got "+result;
---
+++ minifier.ts
@@ -0,0 +1,31 @@
+import * as t from 'babel-types';
+import * as babel from 'babel-core';
+
+const visitor = {
+}
+
+export default function() {
+  return { visitor: visitor };
+}
+
+// Runs this plugin standalone.
+function main() {
+  const filename = process.argv[2];
+  const reserved = process.argv.slice(3);
+  const opts = {
+    plugins: [
+    ],
+    babelrc: false,
+    minified: true
+  };
+  babel.transformFile(filename, opts, (err, result) => {
+    if (err !== null) {
+      throw err;
+    }
+    console.log(result.code);
+  });
+}
+
+if (require.main === module) {
+  main();
+}
