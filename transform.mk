9d6d17a3a898d501b875d86db72d98d896742af7
Rachit Nigam <rachit.nigam12@gmail.com>
2017-09-13 12:26:22 -0400

    Remove dead code.

--- transform.mk
+++
@@ -1,8 +0,0 @@
-# Add a new stopify transform here. The name should correspond to the flag
-# used by the stopify binary. Use '@' to represent spaces when specifying
-# options.
-TRANSFORMS = yield yield@--optimize
-
-# Specify the intervals here.
-INTERVALS = 1000 2000
-# 500 750 1000 1250 1500 2000 2500
