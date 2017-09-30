eae68ca6f8303f148c33c6e6976ff75d79a00af7
Arjun Guha <arjun@cs.umass.edu>
2017-09-30 13:33:55 -0400

    wip

--- harness/R/python_case_study.R
+++ harness/R/python_case_study.R
@@ -3,8 +3,11 @@ suppressMessages(library(tidyverse))
 suppressMessages(source("./theme.R"))
 library(stringr)
 
+# 99% within 2.58all stddev
+plot_ci <- .99
+
 timing_csv <- commandArgs(TRUE)
-timing_csv <- c("results.csv")
+timing_csv <- c("../results.csv")
 
 if (length(timing_csv) != 1) {
   cat("Usage: python_case_study.R <timing.csv>\n")
@@ -13,7 +16,9 @@ if (length(timing_csv) != 1) {
 
 all_data <- read_csv(timing_csv) %>%
   filter(Language == "python_pyjs") %>%
-  select(-Language)
+  select(-Language) %>%
+  mutate(Platform = ifelse(Platform == "MicrosoftEdge", "edge", Platform),
+         NewMethod = ifelse(NewMethod == "direct", "new", "object"))
 
 # Some Python benchmarks do not complete in full ES5 mode in a reasonable
 # amount of time. So, we exclude them from the case study but they are in
@@ -21,6 +26,7 @@ all_data <- read_csv(timing_csv) %>%
 conservative_benchmarks <- unique((all_data %>% filter(EsMode == "es5"))$Benchmark)
 all_data <- all_data %>% filter(Benchmark %in% conservative_benchmarks)
 
+# Native running times.
 native <- all_data %>%
   filter(Platform == "native") %>%
   select(Benchmark,RunningTime) %>%
@@ -28,6 +34,7 @@ native <- all_data %>%
   summarise(MeanNativeTime = mean(RunningTime)) %>%
   ungroup()
 
+# Running times on PyJS without Stopify.
 original <- all_data %>%
   filter(Transform == "original") %>%
   select(Benchmark,Platform,RunningTime)
@@ -37,86 +44,76 @@ original_avgtimes <- original %>%
   group_by(Benchmark, Platform) %>%
   summarise(AvgOriginalTime = mean(RunningTime)) %>%
   ungroup()
+
+original_chrome <- original_avgtimes %>% 
+  filter(Platform == 'chrome') %>%
+  select(-Platform)
+
+ci <- function(vec) {
+  n <- length(vec)
+  return (qt(1 - plot_ci, n - 1) * sd(vec) / sqrt(n))
+}
   
 # Compare vanilla PyJS vs. PyJS + Stopify with naive settings on Chrome
-conservative <- inner_join(
-  all_data %>%
-    filter(Platform == "chrome" & Transform == "lazy" & NewMethod == "direct" &
+conservative <- all_data %>%
+  filter(Platform == "chrome" & Transform == "lazy" & NewMethod == "new" &
            EsMode == "es5" & Estimator == "countdown") %>% 
-    select(Benchmark,RunningTime),
-  original_avgtimes %>% 
-    filter(Platform == 'chrome') %>%
-    select(-Platform))
-conservative <- conservative %>%
+  select(Benchmark,RunningTime) %>%
+  inner_join(original_chrome) %>%
   mutate(Slowdown = RunningTime / AvgOriginalTime) %>%
   select(Benchmark, Slowdown) %>%
   group_by(Benchmark) %>%
-  summarise(MeanSlowdown = mean(Slowdown),
-            SeSlowdown = sd(Slowdown) / sqrt(length(Slowdown))) %>%
+  summarise(Mean = mean(Slowdown), CI = ci(Slowdown)) %>%
   ungroup()
 
-mean(conservative$MeanSlowdown)
-
-plot <- ggplot(conservative, aes(x=Benchmark,y=MeanSlowdown)) +
-  mytheme() +
-  geom_bar(stat="identity",fill="red") +
-  geom_errorbar(aes(ymin=MeanSlowdown-SeSlowdown,ymax=MeanSlowdown+SeSlowdown)) +
-  ylab("Slowdown relative to PyJS")
-mysave("pyjs_case_study_conservative.pdf", plot)
+mean(conservative$Mean)
 
 # Slowdown without implicit conversions
-es_sane <- inner_join(
-  all_data %>%
-    filter(Platform == "chrome" & Transform == "lazy" & NewMethod == "direct" &
-             EsMode == "sane" & Estimator == "countdown") %>%
-    select(Benchmark,RunningTime),
-  original_avgtimes %>% 
-    filter(Platform == 'chrome') %>%
-    select(-Platform))
-es_sane <- es_sane %>%  
+es_sane <- all_data %>%
+  filter(Platform == "chrome" & Transform == "lazy" & NewMethod == "new" &
+         EsMode == "sane" & Estimator == "countdown") %>%
+  select(Benchmark,RunningTime) %>%
+  inner_join(original_chrome, by="Benchmark") %>%
   mutate(Slowdown = RunningTime / AvgOriginalTime) %>%
   select(Benchmark, Slowdown) %>%
   group_by(Benchmark) %>%
-  summarise(MeanSlowdown = mean(Slowdown),
-            SeSlowdown = sd(Slowdown) / sqrt(length(Slowdown))) %>%
+  summarise(Mean = mean(Slowdown), CI = ci(Slowdown)) %>%
   ungroup()
-
-mean(es_sane$MeanSlowdown)
 
 es_sane_vs_insane <- rbind(
   conservative %>% mutate(Factor = "Implicit Conversions"),
   es_sane %>% mutate(Factor = "No Conversions")
 )
 
-plot <- ggplot(es_sane_vs_insane, aes(x=Benchmark,y=MeanSlowdown,fill=Factor)) +
+my_error_bars <- geom_errorbar(
+  aes(ymin=Mean-CI/2,ymax=Mean+CI/2), size=0.1, width=.9, 
+      position=position_dodge(.9))
+
+plot <- ggplot(es_sane_vs_insane, aes(x=Benchmark,y=Mean,fill=Factor)) +
   mytheme() +
   geom_bar(stat="identity", position="dodge") +
-  geom_errorbar(aes(ymin=MeanSlowdown-SeSlowdown,ymax=MeanSlowdown+SeSlowdown)) +
+  my_errors_bars +
   ylab("Slowdown relative to PyJS")
 mysave("pyjs_case_study_sane_vs_insane.pdf", plot)
 
 new_method <- all_data %>%
   filter(Transform == "lazy" & EsMode == "sane" & Estimator == "countdown") %>%
-  select(Benchmark,Platform,NewMethod,RunningTime)
-new_method <- inner_join(new_method,original_avgtimes) %>%
-  mutate(Factor = str_c(Platform, 
-                        ifelse(NewMethod == "direct", "new", "Object.c"),
-                        sep = " - ")) %>%
-  select(Benchmark,Factor,RunningTime, AvgOriginalTime)
-
-new_method <- new_method %>%
+  select(Benchmark,Platform,NewMethod,RunningTime) %>%
+  inner_join(original_avgtimes, by=c("Benchmark", "Platform")) %>%
+  filter(Platform == "chrome" | Platform == "edge") %>%
+  mutate(Factor = str_c(Platform, NewMethod, sep = " - ")) %>%
+  select(Benchmark,Factor,RunningTime, AvgOriginalTime) %>%
   mutate(Slowdown = RunningTime / AvgOriginalTime) %>%
   select(Benchmark, Factor, Slowdown) %>%
   group_by(Benchmark,Factor) %>%
-  summarise(MeanSlowdown = mean(Slowdown),
-            SeSlowdown = sd(Slowdown) / sqrt(length(Slowdown))) %>%
+  summarise(Mean = mean(Slowdown), CI = ci(Slowdown)) %>%
   ungroup()
 
 plot <- ggplot(new_method, 
-               aes(x=Benchmark,y=MeanSlowdown,fill=Factor)) +
+               aes(x=Benchmark,y=Mean,fill=Factor)) +
   mytheme() +
   geom_bar(stat="identity", position="dodge") +
-  geom_errorbar(aes(ymin=MeanSlowdown-SeSlowdown,ymax=MeanSlowdown+SeSlowdown)) +
+  my_error_bars +
   ylab("Slowdown relative to PyJS")
 mysave("pyjs_case_study_new_method.pdf", plot)
 
--- harness/R/theme.R
+++ harness/R/theme.R
@@ -1,16 +1,14 @@
 suppressMessages(library(tidyverse))
 library(stringr)
-
-library(showtext)
-font_add("lm", "fonts/lmroman9-regular.otf")
-showtext_auto()
+library(extrafont)
+library(fontcm)
 
 # Theme for output
 mytheme <- function() {
   return(theme_bw() +
            theme(
              panel.background = element_rect(size = 0.9),
-             text = element_text(family = "lm", size=10),
+             text = element_text(family = "CM Roman", size=10),
              #panel.grid.major = element_blank(),
              #panel.grid.minor = element_blank(),
              panel.grid.major = element_line(colour="gray", size=0.1),
@@ -18,18 +16,16 @@ mytheme <- function() {
                element_line(colour="gray", size=0.1, linetype='dotted'),
              axis.ticks = element_line(size=0.05),
              axis.ticks.length=unit("-0.05", "in"),
-             axis.ticks.margin=unit("0.2", "in"),
              axis.text.y = element_text(margin = margin(r = 5)),
-             axis.text.x = element_text(margin = margin(t = 5), angle=90),
+             axis.text.x = element_text(hjust=1, angle=45),
              legend.key = element_rect(colour=NA),
              legend.margin = unit(0.001, "in"),
              legend.key.size = unit(0.2, "in"),
              legend.title = element_blank(),
              legend.position = c(0.9, .7),
-             legend.text = element_text(family = "lm", size=9),
              legend.background = element_blank()))
 }
 
 mysave <- function(filename, plot) {
-  ggsave(filename, plot, width=8, height=8, units=c("in"))
+  ggsave(filename, plot, width=3, height=3, units=c("in"))
 }
