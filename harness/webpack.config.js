1e331fcc9488005448f9e924f23c30f6ee4d33d2
Sam Baxter <sbaxter@cs.umass.edu>
2017-10-25 21:14:47 -0400

    Fix webpacking commander deps for benchmarkClient

--- harness/webpack.config.js
+++ harness/webpack.config.js
@@ -2,5 +2,9 @@ module.exports = {
   entry: './dist/benchmarkClient.js',
   output: {
     filename: 'www/benchmarkClient.js'
+  },
+  node: {
+    'fs': 'empty',
+    'child_process': 'empty',
   }
 };
