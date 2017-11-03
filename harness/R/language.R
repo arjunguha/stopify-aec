ad268cc4e613ec1edfdb0ad2103c345c715c9992
Arjun Guha <arjun@cs.umass.edu>
2017-11-03 19:12:49 -0400

    fixed

--- harness/R/language.R
+++ harness/R/language.R
@@ -12,15 +12,32 @@ original_avgtimes <- original %>%
   summarise(AvgOriginalTime = mean(RunningTime)) %>%
   ungroup()
 
+by_platform <- function(platform) {
+  slowdowns <- all_data %>%
+    filter(Transform != "original") %>%
+    filter(Estimator == "reservoir") %>%
+    filter(Platform == platform) %>%
+    filter(YieldInterval == 100) %>%
+    inner_join(original_avgtimes) %>%
+    mutate(Slowdown = RunningTime / AvgOriginalTime) %>%
+    filter(Slowdown < 100) %>%
+    select(Benchmark,Language,Slowdown) %>%
+    mutate(N = 1) %>%
+    arrange(Slowdown) %>%
+    mutate(Y = cumsum(N))
+  return(slowdowns)
+  
+}
+
 by_language <- function(lang) {
   slowdowns <- all_data %>%
     filter(Transform != "original") %>%
     filter(Estimator == "reservoir") %>%
     filter(Language == lang) %>%
     filter(YieldInterval == 100) %>%
-    select(Benchmark,Platform,RunningTime) %>%
     inner_join(original_avgtimes) %>%
     mutate(Slowdown = RunningTime / AvgOriginalTime) %>%
+    select(Benchmark,Platform,Slowdown) %>%
     mutate(N = 1) %>%
     arrange(Slowdown) %>%
     mutate(Y = cumsum(N))
@@ -33,6 +50,16 @@ save_by_language <- function(lang) {
   ggsave(paste0(lang, ".pdf"), plot)
 }
 
+save_by_platform <- function(platform) {
+  plot <- ggplot(by_platform(platform), aes(x=Slowdown,y=Y,color=Language)) + 
+    geom_step()
+  ggsave(paste0(platform, ".pdf"), plot)
+}
+
+save_by_platform("chrome")
+save_by_platform("firefox")
+save_by_platform("safari")
+save_by_platform("MicrosoftEdge")
 save_by_language("python_pyjs") # 1
 save_by_language("c++") # 2
 save_by_language("dart_dart2js") # 3
