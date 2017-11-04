d2771fbb2383b045727af1ed966359d74cfd6c0a
Arjun Guha <arjun@cs.umass.edu>
2017-11-04 10:02:33 -0400

    x

--- harness/R/language.R
+++ harness/R/language.R
@@ -1,5 +1,33 @@
+#!/usr/bin/env Rscript
 suppressMessages(library(tidyverse))
 library(stringr)
+library(extrafont)
+library(fontcm)
+
+palette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# Theme for output
+mytheme <- function() {
+  return(theme_bw() +
+           theme(
+             panel.background = element_rect(size = 0.9),
+             text = element_text(family="serif", size=16),
+             #panel.grid.major = element_blank(),
+             #panel.grid.minor = element_blank(),
+             panel.grid.major = element_line(colour="gray", size=0.1),
+             panel.grid.minor =
+               element_line(colour="gray", size=0.1, linetype='dotted'),
+             axis.ticks = element_line(size=0.05),
+             axis.ticks.length=unit("-0.05", "in"),
+             axis.text.y = element_text(margin = margin(r = 5)),
+             axis.text.x = element_text(margin = margin(t = 5)),
+             legend.key = element_rect(colour=NA),
+             legend.margin = margin(unit(0.001, "in")),
+             legend.key.size = unit(0.2, "in"),
+             legend.title = element_blank(),
+             legend.position = c(0.7, .2),
+             legend.background = element_blank()))
+}
 
 all_data <- read_csv("../results.csv")
 
@@ -11,6 +39,20 @@ original_avgtimes <- original %>%
   group_by(Benchmark, Language, Platform) %>%
   summarise(AvgOriginalTime = mean(RunningTime)) %>%
   ungroup()
+
+slowdowns <- all_data %>%
+  filter(Transform != "original") %>%
+  filter(Estimator == "reservoir") %>%
+  filter(YieldInterval == 100) %>%
+  inner_join(original_avgtimes) %>%
+  mutate(Slowdown = RunningTime / AvgOriginalTime) %>%
+  select(Benchmark,Platform,Language,Slowdown) %>%  
+  mutate(N = 1) %>%
+  group_by(Platform, Language) %>%
+  arrange(Slowdown) %>%
+  mutate(.y = cumsum(N) / length(N)) %>%
+  ungroup() %>%
+  select(-N)
 
 by_platform <- function(platform) {
   slowdowns <- all_data %>%
@@ -30,36 +72,20 @@ by_platform <- function(platform) {
 }
 
 by_language <- function(lang) {
-  slowdowns <- all_data %>%
-    filter(Transform != "original") %>%
-    filter(Estimator == "reservoir") %>%
+  slowdowns <- slowdowns %>%
     filter(Language == lang) %>%
-    filter(YieldInterval == 100) %>%
-    inner_join(original_avgtimes) %>%
-    mutate(Slowdown = RunningTime / AvgOriginalTime) %>%
-    select(Benchmark,Platform,Slowdown) %>%
-    mutate(N = 1) %>%
-    arrange(Slowdown) %>%
-    mutate(Y = cumsum(N))
   return(slowdowns)
 }
 
 save_by_language <- function(lang) {
-  plot <- ggplot(by_language(lang), aes(x=Slowdown,y=Y,color=Platform)) + 
-    geom_step()
-  ggsave(paste0(lang, ".pdf"), plot)
+  plot <- ggplot(slowdowns %>% filter(Language==lang), 
+                 aes(x=Slowdown,color=Platform)) + 
+    stat_ecdf() +
+    mytheme() +
+    ylab("Percentage of Benchmarks")
+  ggsave(paste0(lang, ".pdf"), plot, width=4, height=3, units=c("in"))
 }
 
-save_by_platform <- function(platform) {
-  plot <- ggplot(by_platform(platform), aes(x=Slowdown,y=Y,color=Language)) + 
-    geom_step()
-  ggsave(paste0(platform, ".pdf"), plot)
-}
-
-save_by_platform("chrome")
-save_by_platform("firefox")
-save_by_platform("safari")
-save_by_platform("MicrosoftEdge")
 save_by_language("python_pyjs") # 1
 save_by_language("c++") # 2
 save_by_language("dart_dart2js") # 3
