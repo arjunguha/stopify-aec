07df28513a66a8fd7104b5672ba99ff4ac81a4d0
Sam Baxter <sbaxter@cs.umass.edu>
2018-02-18 17:28:38 -0500

    Add skulpt precompiled files

--- skulpt/compile.sh
+++ skulpt/compile.sh
@@ -5,7 +5,7 @@ if [ ! -d js-build/ ]; then
   mkdir js-build
 fi
 
-for x in anagram b binary_trees fib gcbench nbody pystone raytrace_simple richards schulze spectral_norm
+for x in anagram binary_trees fib gcbench nbody pystone schulze spectral_norm
 do
   echo $x
   node html-generator.js $x/$x.js js-build/$x
